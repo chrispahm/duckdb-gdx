d571eab51d4a62150875c7b5286d0d19919cd6f5
Christoph Pahmeyer <christoph.pahmeyer@ilr.uni-bonn.de>
2025-12-15 08:33:36 +0100

    :zap: add a minimal python test and docs for potential large file exploration

--- test/integration/minimal_query_test.py
+++ test/integration/minimal_query_test.py
@@ -12,18 +12,25 @@ extension_path = "/Users/pahmeyer/Documents/GitHub.nosync/duckdb-gdx/build/relea
 conn = duckdb.connect(config={"allow_unsigned_extensions": True})
 conn.execute(f"LOAD '{extension_path}'")
 
+# query = f"""
+#     SELECT * FROM read_gdx('{gdx_path}', 'p_faoTradeMatrix')
+# """
 query = f"""
-    SELECT * FROM read_gdx('{gdx_path}', 'p_faoTradeMatrix')
-    WHERE dim_1 = '1' AND dim_2 = '2' AND dim_3 = '561' AND dim_4 = 'Import'
+COPY (
+    SELECT *
+    FROM read_gdx('{gdx_path}', 'p_faoTradeMatrix')
+)
+TO 'p_faoTradeMatrix.parquet'
+(FORMAT PARQUET, OVERWRITE TRUE);
 """
-
+# WHERE dim_1 = '1' AND dim_2 = '2' AND dim_3 = '561' AND dim_4 = 'Import'
 start = time.perf_counter()
 result = conn.execute(query).fetchall()
 elapsed = (time.perf_counter() - start) * 1000
 
 print(f"Query took {elapsed:.2f} ms")
 print(f"Rows: {len(result)}")
-for row in result:
-    print(row)
+# for row in result:
+#    print(row)
 
 conn.close()
